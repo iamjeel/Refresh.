c3bc6a6b4f6a23a9edf443f872927b9c821ae979
Jeel Thumar <thumar.jeel@gmail.com>
2022-03-08 14:36:30 +0530

    Now we can change bg color on refresh

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import Main from './components/main'
+import { useState } from 'react';
+import useContextMenu from './components/useContextMenu';
+import './assets/App.css';
 
 function App() {
-  return (
-    <div>
-      <h1>Right click somewhere on the page..</h1>
-      <Main />
-    </div>
-  );
+  const { anchorPoint, show } = useContextMenu();
+
+  const randomColors = [
+    { color: '#ACADBC' },
+    { color: '#9B9ECE' },
+    { color: '#473BF0' },
+    { color: '#2D7DD2' },
+    { color: '#EEB902' },
+    { color: '#97CC04' },
+    { color: '#F45D01' },
+    { color: '#92817A' },
+    { color: '#F1DABF' },
+    { color: '#FFFBFF' },
+    { color: '#297373' },
+    { color: '#89B0AE' },
+    { color: '#BEE3DB' },
+    { color: '#FFD6BA' },
+    { color: '#E6AF2E' },
+  ];
+  const [colorValue, setColorValue] = useState(0);
+
+  const handleClick = () =>
+    setColorValue(colorValue + 1 < randomColors.length ? colorValue + 1 : 0);
+  if (show) {
+    return (
+      <div
+        className="app"
+        style={{
+          backgroundColor: randomColors[colorValue].color,
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <h1>Refresh to Change the Background Color..</h1>
+        <ul
+          className="main"
+          style={{ top: anchorPoint.y, left: anchorPoint.x }}
+          onClick={handleClick}
+        >
+          <li>Refresh</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="app"
+        style={{
+          backgroundColor: randomColors[colorValue].color,
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <h1>Refresh to Change the Background Color..</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
